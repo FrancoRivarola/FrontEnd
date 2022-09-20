2b49a87ca436eddabf809a14df555ea65f85f76f
FrancoRivarola <francoellevita@hotmail.com>
2022-09-20 16:54:14 -0300

    Componente Educacion

--- ProyectoFinal/FrontEnd/src/app/app-routing.module.ts
+++ ProyectoFinal/FrontEnd/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
+import { EditExperienciaComponent } from './components/experiencias/edit-experiencia.component';
+import { NuevaExperienciaComponent } from './components/experiencias/nueva-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'nuevaexp', component: NuevaExperienciaComponent },
+  { path: 'editexp/:id', component: EditExperienciaComponent },
+  { path: 'nuevaedu', component: NeweducacionComponent },
 ];
 
 @NgModule({
--- ProyectoFinal/FrontEnd/src/app/app.module.ts
+++ ProyectoFinal/FrontEnd/src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { BannerComponent } from './banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { ExperienciaComponent } from './components/experiencias/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HComponent } from './components/skills/h.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { interceptorProvider } from './service/interceptor-service';
+import { NuevaExperienciaComponent } from './components/experiencias/nueva-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencias/edit-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 
 @NgModule({
   declarations: [
@@ -31,14 +36,20 @@ import { LoginComponent } from './components/login/login.component';
     FooterComponent,
     HomeComponent,
     LoginComponent,
+    NuevaExperienciaComponent,
+    EditExperienciaComponent,
+    NeweducacionComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     NgCircleProgressModule.forRoot({}),
+    FormsModule,
     HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [interceptorProvider],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
