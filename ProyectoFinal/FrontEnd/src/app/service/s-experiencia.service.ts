c46f048641af7bcd3add3578057813fced586423
FrancoRivarola <francoellevita@hotmail.com>
2022-09-21 02:14:18 -0300

    cors

--- ProyectoFinal/FrontEnd/src/app/service/s-experiencia.service.ts
+++ ProyectoFinal/FrontEnd/src/app/service/s-experiencia.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root',
 })
 export class SExperienciaService {
-  expURL = 'https://argprobe.herokuapp.com/explab';
+  expURL = 'https://argprobe.herokuapp.com/explab/';
 
   constructor(private httpClient: HttpClient) {}
 
